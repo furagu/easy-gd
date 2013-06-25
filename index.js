0397fb639cfd78ea8b7e9752d3ce4e408428308f
Egor Balyshev <frag.trials@gmail.com>
2013-06-26 01:24:53 +0800

    Cleaned copypasted piece of code

--- index.js
+++ index.js
@@ -163,9 +163,8 @@ function chunk (arr, len) {
     var chunks = [],
         i = 0,
         n = arr.length
-    while (i < n) {
-        chunks.push(arr.slice(i, i += len));
-    }
+    while (i < n)
+        chunks.push(arr.slice(i, i += len))
     return chunks
 }
 
