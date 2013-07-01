083341e009a708781eacc30e48f40ea6df7c203e
Egor Balyshev <frag.trials@gmail.com>
2013-07-02 00:02:39 +0800

    Removed unneeded test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var gd = require('./index.js')
-
-gd.createTrueColor(100, 100).saveJpeg('test.jpg')
-
-gd.createFrom('test.jpg', function (err, image) {
-    console.log(err, image.width, image.height)
-})
