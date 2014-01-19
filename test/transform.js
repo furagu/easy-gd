60d5bdb9c6e6a6473525338fdc002ecd29c93c8e
Egor Balyshev <frag.trials@gmail.com>
2014-01-20 00:48:52 +0800

    Transform interface tests

--- test/transform.js
+++ test/transform.js
@@ -1,6 +1,7 @@
 var should = require('should'),
     gd = require('../index.js'),
     _ = require('underscore'),
+    async = require('async'),
     fs = require('fs'),
     stream = require('stream'),
     samples = require('./samples.js'),
@@ -29,20 +30,88 @@ describe('gd', function () {
         )
     })
 
-    it('should .watermark() images piped in')
-    it('should set format setting for piped in images with .format()')
-    it('should set quality setting for piped in images with .quality()')
-    it('should set compression setting for piped in images with .compression()')
-    it('should set saving options for piped in images with .options()')
+    it('should .watermark() images piped in', function (done) {
+        testTransform(
+            gd.watermark(samples.watermark, {x: 0, y: 0}),
+            function (image) {
+                image.getPixel(1, 1).should.equal(0xFFFFFF)
+                done()
+            }
+        )
+    })
+
+    it('should set format setting for piped in images with .format()', function (done) {
+        async.each(samples.types, function (type, doneType) {
+            testTransform(
+                gd.format(type),
+                function (image) {
+                    image.format.should.equal(type)
+                    doneType()
+                }
+            )
+        }, done)
+    })
+
+    it('should set quality setting for piped in images with .quality()', function (done) {
+        async.parallel({
+            highQualityImage: function (callback) {
+                testTransform(
+                    gd.format('jpeg').quality(100),
+                    function (image, data) {callback(null, data)}
+                )
+            },
+            lowQualityImage: function (callback) {
+                testTransform(
+                    gd.format('jpeg').quality(10),
+                    function (image, data) {callback(null, data)}
+                )
+            }
+        }, function (err, results) {
+            results.highQualityImage.length.should.be.above(results.lowQualityImage.length)
+            done()
+        })
+    })
+
+    it('should set compression setting for piped in images with .compression()', function (done) {
+        async.parallel({
+            poorlyCompressedImage: function (callback) {
+                testTransform(
+                    gd.format('png').compression(1),
+                    function (image, data) {callback(null, data)}
+                )
+            },
+            heavilyCompressedImage: function (callback) {
+                testTransform(
+                    gd.format('png').compression(9),
+                    function (image, data) {callback(null, data)}
+                )
+            }
+        }, function (err, results) {
+            results.poorlyCompressedImage.length.should.be.above(results.heavilyCompressedImage.length)
+            done()
+        })
+    })
+
+    it('should set saving options for piped in images with .options()', function (done) {
+        async.each(samples.types, function (type, doneType) {
+            testTransform(
+                gd.options({format :type}),
+                function (image) {
+                    image.format.should.equal(type)
+                    doneType()
+                }
+            )
+        }, done)
+    })
 })
 
 function testTransform(transform, callback) {
     var dst = h.WritableStream()
     dst.on('finish', function () {
         var image = gd.open(this.written)
-        callback(image)
+        callback(image, this.written)
     })
-    var imageData = h.createImage().save({format: 'png'})
+    var imageData = h.createImage(100, 100).save({format: 'png'})
     var src = new h.ReadableStream(imageData)
     src.pipe(transform).pipe(dst)
 }
